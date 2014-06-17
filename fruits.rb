7d4bd6469521c6df375ce61e6bbe114b3e0ec4eb
Alex McRitchie <amcritchie@gmail.com>
2014-06-17 11:57:01 -0600

    Adding so far

--- fruits.rb
+++ fruits.rb
@@ -1,1 +1,18 @@
 fruits = ['apples', 'oranges', 'bananas', 'pomegranates', 'grapes']
+
+puts fruits.length
+puts '>'*40
+puts fruits[2]
+puts '>'*40
+puts fruits.insert(1, 'rasberries')
+puts '>'*40
+x = 0
+while x < 6
+  puts fruits[x].length
+  x = x + 1
+end
+puts '>'*40
+
+# puts ['a','b','c'].each {|elem|elem.upcase}
+
+puts fruits.map(&:upcase)
